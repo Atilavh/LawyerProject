c9903739b44002e3a2e39522f99b573cb8855f87
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-02 18:51:22 +0330

    Update urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='home'),
-    # path('', views.home_page, name='home_page'),
+    path('LoginPage', views.LoginPage, name='Login'),
+    path('', views.index, name='home_page')
+    # <--This urls for django_render_partial
+    # path('site-header', views.header_render_partial, 'header_render_partial'),
+    # path('site-footer', views.footer_render_partial, 'footer_render_partial')
+    # -->
 ]
